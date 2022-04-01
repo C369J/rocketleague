aeb202692ad7859e56a7999e5871656274149d9b
C369J <63129567+C369J@users.noreply.github.com>
2022-04-01 14:35:52 -0700

    Update and rename index.htm to main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+<javascript>
+
+  var sy=0;
+  var sx=0;
+  var x=0;
+  var y=0;
+
+  Document.createElement('player');
+
+for() {
+  
+  sy --;
+  sy -= sy;
+  player.style.left=sx;
+  player.style.top=sy;
+  
+  }
+
+</javascript>
